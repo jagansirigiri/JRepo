54e656e712c078eb944b6806c1dd9685768bea1e
Jagan Sirigiri <jagansirigiri@gmail.com>
2023-08-19 19:14:53 -0400

    Updated one statement and added another new statement.

--- src/com/jagan/JHelloWorld.java
+++ src/com/jagan/JHelloWorld.java
@@ -2,8 +2,8 @@ package com.jagan;
 public class JHelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		System.out.println("Hello World!Jagan");
+		System.out.println("Git practice");
 	}
 
 }
